0c76016fd1d29101addafcb4d047b965b90adeee
EduS <55415480+eduSobrino@users.noreply.github.com>
2026-04-18 21:16:36 +0200

    Implement get_next_line bonus with multi-fd support

---
+++ get_next_line_bonus.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esobrino <esobrino@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/12 21:13:08 by esobrino          #+#    #+#             */
+/*   Updated: 2026/04/18 21:12:33 by esobrino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+static int	resolve_line_len(int fd, char **stash, ssize_t *line_len);
+static char	*solve_and_trim(char **stash, ssize_t line_len);
+
+char	*get_next_line(int fd)
+{
+	static char	*stash[MAX_FD];
+	ssize_t		line_len;
+	int			status;
+
+	if (fd < 0 || fd >= MAX_FD || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (read(fd, 0, 0) < 0)
+	{
+		clear_stash(&stash[fd]);
+		return (NULL);
+	}
+	status = resolve_line_len(fd, &stash[fd], &line_len);
+	if (status <= 0)
+		return (NULL);
+	return (solve_and_trim(&stash[fd], line_len));
+}
+
+int	get_len(char *s)
+{
+	int	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (*(s + i) != '\0')
+		i++;
+	return (i);
+}
+
+void	clear_stash(char **stash)
+{
+	free(*stash);
+	*stash = NULL;
+}
+
+static char	*solve_and_trim(char **stash, ssize_t line_len)
+{
+	char		*line;
+
+	line = solve_line(*stash, line_len);
+	if (!line)
+	{
+		clear_stash(stash);
+		return (NULL);
+	}
+	if (trim_stash(stash, line) < 0)
+	{
+		free(line);
+		clear_stash(stash);
+		return (NULL);
+	}
+	return (line);
+}
+
+static int	resolve_line_len(int fd, char **stash, ssize_t *line_len)
+{
+	ssize_t		read_bytes;
+
+	*line_len = find_trigger(*stash);
+	read_bytes = 1;
+	while (*line_len == -1 && read_bytes > 0)
+	{
+		read_bytes = extract(fd, stash);
+		if (read_bytes < 0)
+		{
+			clear_stash(stash);
+			return (-1);
+		}
+		*line_len = find_trigger(*stash);
+	}
+	if (*line_len == -1 && read_bytes == 0)
+	{
+		if (get_len(*stash) == 0)
+			return (0);
+		*line_len = get_len(*stash);
+	}
+	return (1);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esobrino <esobrino@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/16 18:02:08 by esobrino          #+#    #+#             */
+/*   Updated: 2026/04/18 21:11:41 by esobrino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 100
+# endif
+
+# ifndef TRIGGER
+#  define TRIGGER '\n'
+# endif
+
+# if TRIGGER < 0 || TRIGGER > 127 || TRIGGER == '\0'
+#  error "TRIGGER must be a non-NUL ASCII character"
+# endif
+
+# define MAX_FD 1024
+
+/* -----MAIN----------------------------------------------------------------- */
+char	*get_next_line(int fd);
+
+/* -----UTILS---------------------------------------------------------------- */
+int		find_trigger(char *stash);
+ssize_t	extract(int fd, char **stash);
+int		append_stash(char **stash, char *buffer);
+int		trim_stash(char **stash, char *line);
+char	*solve_line(char *stash, int len);
+int		get_len(char *s);
+void	clear_stash(char **stash);
+
+#endif
